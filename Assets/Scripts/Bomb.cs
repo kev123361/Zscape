06213da8d8fb5afdc62cd505f238551abaee4949
kevin400085 <kevin400085@yahoo.com>
2019-03-07 13:01:37 -0500

    Enemies can throw bombs in an arc

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -7,6 +7,7 @@ public class Bomb : MonoBehaviour
     public Vector2Int explosionPos;
     public BoardManager bm;
     public GameObject explosion;
+    public Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
@@ -29,5 +30,21 @@ public class Bomb : MonoBehaviour
             newExplosion.GetComponent<Explosion>().bm = bm;
             Destroy(gameObject);
         }
+    }
+
+    public void SetTarget(Vector2Int newExplosionPos)
+    {
+        explosionPos = newExplosionPos;
+    }
+
+    public void SetBoard(BoardManager newBM)
+    {
+        bm = newBM;
+    }
+
+    public void SetRigidBody(Rigidbody newRB)
+    {
+        rb = newRB;
+        rb.velocity = new Vector3(0, 4, -8);
     }
 }
---
+++ Assets/Scripts/BombEnemy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombEnemy : Enemy
+{
+    new private float timer = 0f;
+    private Vector2Int target;
+    public Bomb bomb;
+    new public static event EnemyDeath OnEnemyDeath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bm = board.GetComponent<BoardManager>();
+        //boardSize = bm.tiles.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target.Set(pos.x, pos.y - 4);
+        if (health <= 0)
+        {
+            Die();
+        }
+        timer += Time.deltaTime;
+        if (timer >= timeToShoot)
+        {
+            Shoot();
+            timer = 0f;
+        }
+    }
+
+    new private void Shoot()
+    {
+        var newBomb = Instantiate(bomb, transform.position + (transform.up * 2), transform.rotation);
+        newBomb.SetTarget(target);
+        newBomb.SetBoard(bm);
+        newBomb.SetRigidBody(newBomb.GetComponent<Rigidbody>());
+    }
+
+    // Before deleting gameObject, make call to enemy manager to decrement enemy count
+    private void Die()
+    {
+        //GameObject.FindGameObjectWithTag("Enemy Manager").GetComponent<EnemyManager>().EnemyDied();
+        if (OnEnemyDeath != null)
+        {
+            OnEnemyDeath();
+        }
+        Destroy(gameObject);
+    }
+}
