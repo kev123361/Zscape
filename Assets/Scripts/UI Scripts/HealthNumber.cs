acf95f95de62f9d4dcbddb2e0173065c9639659a
unknown <Triplett.Seth@gmail.com>
2019-04-10 19:59:38 -0400

    Added damage numbers.

--- Assets/Scripts/UI Scripts/HealthNumber.cs
+++ Assets/Scripts/UI Scripts/HealthNumber.cs
@@ -5,7 +5,9 @@ using UnityEngine.UI;
 
 public class HealthNumber : MonoBehaviour
 {
-    public Text text;
+    public Text healthText;
+    public Text damageText;
+    public Text fadingDamageText;
     public Slider slider;
     public RectTransform whiteHealthRect;
     public RectTransform fadingWhiteHealthRect;
@@ -19,14 +21,22 @@ public class HealthNumber : MonoBehaviour
     private float fadingHealthEnd = 0f;
     private float whiteHealthTimer = 0f;
     private float fadingHealthTimer = 0f;
+    private float currentDamage = 0f;
     private const float baseAppearTime = 0.75f;
     private const float baseFadeTime = 0.5f;
+    private static Color RED = new Color(0.849f, 0.11886f, 0.11886f);
+    private static Color YELLOW = new Color(0.9433416f, 0.95f, 0.13965f);
 
     void Awake()
     {
-        if (text == null)
+        // Only player health bar will have health text, so it can be null on enemies
+        if (damageText == null)
         {
             Debug.LogError("No health_text found.");
+        }
+        if (fadingDamageText == null)
+        {
+            Debug.LogError("No fading_damage_text found.");
         }
         if (slider == null)
         {
@@ -55,6 +65,13 @@ public class HealthNumber : MonoBehaviour
                 fadingHealthTimer = baseFadeTime;
                 whiteHealthStart = 0f;
                 whiteHealthEnd = 0f;
+                // set fading damage and reset current damage
+                fadingDamageText.text = currentDamage.ToString();
+                fadingDamageText.enabled = true;
+                fadingDamageText.color = damageText.color;
+                damageText.enabled = false;
+                damageText.color = RED;
+                currentDamage = 0f;
             }
             whiteHealthRect.anchorMin = new Vector2(whiteHealthStart, 0);
             whiteHealthRect.anchorMax = new Vector2(whiteHealthEnd, 1);
@@ -66,6 +83,7 @@ public class HealthNumber : MonoBehaviour
             fadingHealthTimer -= Time.deltaTime;
             // Move fading health upward
             fadingWhiteHealthRect.anchoredPosition = new Vector2(0.5f, 100 * Mathf.Pow((1f - (fadingHealthTimer / baseFadeTime)), 2f));
+            //fadingDamageText.rectTransform.position = new Vector3(0.5f, 100 * Mathf.Pow((1f - (fadingHealthTimer / baseFadeTime)), 2f), 0f); 
             // Fade the health over time
             fadingHealthFill.color = new Color(fadingHealthFill.color.r, fadingHealthFill.color.g, fadingHealthFill.color.b, (fadingHealthTimer / baseFadeTime));
             // if fade timer runs out
@@ -73,17 +91,24 @@ public class HealthNumber : MonoBehaviour
             {
                 fadingHealthStart = 0f;
                 fadingHealthEnd = 0f;
-                //fadingWhiteHealthRect.localPosition = Vector3.zero;
+                fadingDamageText.enabled = false;
             }
             fadingWhiteHealthRect.anchorMin = new Vector2(fadingHealthStart, 0);
             fadingWhiteHealthRect.anchorMax = new Vector2(fadingHealthEnd,  1);
         }
     }
-
+    
     public void UpdateHealth(float health, float maxHealth, float damage)
     {
-        // Set text to whole number
-        text.text = ((int) health).ToString();
+        UpdateHealth(health, maxHealth, damage, false);
+    }
+
+    public void UpdateHealth(float health, float maxHealth, float damage, bool critical)
+    {
+        // Set health text on player
+        if (healthText != null) {
+            healthText.text = ((int) health).ToString();
+        }
         float proportion = health / maxHealth;
         // Set value based on proportion of health
         slider.value = proportion;
@@ -101,6 +126,32 @@ public class HealthNumber : MonoBehaviour
             }
             whiteHealthStart = health / maxHealth;
             whiteHealthTimer = baseAppearTime;
+            // Add taken damage to current damage
+            currentDamage += damage;
+            damageText.enabled = true;
+            if (critical) {
+                damageText.color = YELLOW;
+            }
+            StartCoroutine(DamageNumberShake());
+        }
+        // Set damage text to whole number
+        damageText.text = ((int) currentDamage).ToString();
+    }
+
+    private IEnumerator DamageNumberShake()
+    {
+        // In radians
+        float randomDirection = Random.Range(0, 2 * Mathf.PI);
+        float sinusoidalMagnitude;
+        float decayingMagnitude = 25f;
+        for (int i = 0; i < 30; i++) {
+            sinusoidalMagnitude = Mathf.Sin(15 * i / (2 * Mathf.PI));
+            // decay magnitude
+            decayingMagnitude = Mathf.Lerp(decayingMagnitude, 0, 0.15f);
+            damageText.rectTransform.localPosition = new Vector3((decayingMagnitude * sinusoidalMagnitude * Mathf.Cos(randomDirection)),
+                                                                 (decayingMagnitude * sinusoidalMagnitude * Mathf.Sin(randomDirection)),
+                                                                 0);
+            yield return new WaitForSeconds(1/60f);
         }
     }
 
