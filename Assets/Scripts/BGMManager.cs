970f240e12ffa7d62bb01508fc68ae8642ecde0b
kevin400085 <kevin400085@yahoo.com>
2019-04-10 21:27:01 -0400

    Music switches between certain states

---
+++ Assets/Scripts/BGMManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour
+{
+
+    public AudioClip[] bgm;
+    private BoardManager board;
+    private AudioSource audioPlayer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+        board = GameObject.Find("Board").GetComponent<BoardManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void playTrack(int i)
+    {
+        audioPlayer.clip = bgm[i];
+        audioPlayer.Play();
+    }
+
+    public void checkLevel()
+    {
+        if (board.level == 3)
+        {
+            playTrack(2);
+        }
+    }
+}
